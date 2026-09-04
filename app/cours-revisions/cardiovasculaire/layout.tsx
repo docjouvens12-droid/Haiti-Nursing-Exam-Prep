202d730042fb4f0546c7e2392ec34ba8649abf6f
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-04 05:44:05 -0600

    Affiche le cours HTA enrichi

--- app/cours-revisions/cardiovasculaire/layout.tsx
+++ app/cours-revisions/cardiovasculaire/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import ObjectifsCardiovasculairePortal from "@/components/ObjectifsCardiovasculairePortal";
 import CardiovascularAnatomyDiagrams from "@/components/CardiovascularAnatomyDiagrams";
+import HTAEnrichie from "@/components/HTAEnrichie";
 
 export default function CardiovasculaireLayout({ children }: { children: ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function CardiovasculaireLayout({ children }: { children: ReactNo
       {children}
       <ObjectifsCardiovasculairePortal />
       <CardiovascularAnatomyDiagrams />
+      <HTAEnrichie />
     </>
   );
 }
