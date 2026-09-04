b878391e7e243efb4b6ae4ebe9b08eb541592673
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-04 03:15:11 -0600

    Aligne les catégories de pratique sur le cursus infirmier haïtien

--- lib/categories.ts
+++ lib/categories.ts
@@ -1,19 +1,27 @@
 export const CATEGORIES_QUESTIONS = [
+  "Soins fondamentaux",
   "Médecine-Chirurgie",
   "Maternité-Obstétrique",
   "Pédiatrie",
   "Pharmacologie",
   "Santé mentale / Psychiatrie",
-  "Soins fondamentaux",
-  "Urgences",
   "Santé communautaire",
-  "Nutrition",
-  "Gériatrie",
-  "Maladies infectieuses",
-  "Soins critiques / Réanimation",
-  "Éthique et déontologie",
-  "Administration et gestion infirmière",
-  "Calculs de médicaments / IV",
+  "Urgences",
 ] as const;
 
 export type CategorieQuestion = (typeof CATEGORIES_QUESTIONS)[number];
+
+export const CATEGORIE_LABELS: Record<string, string> = {
+  "Soins fondamentaux": "Fondements et soins infirmiers de base",
+  "Médecine-Chirurgie": "Soins infirmiers médico-chirurgicaux",
+  "Maternité-Obstétrique": "Santé maternelle, obstétrique et néonatale",
+  "Pédiatrie": "Soins infirmiers pédiatriques",
+  "Pharmacologie": "Pharmacologie et administration des médicaments",
+  "Santé mentale / Psychiatrie": "Santé mentale et soins psychiatriques",
+  "Santé communautaire": "Santé communautaire et santé publique",
+  "Urgences": "Urgences et soins critiques",
+};
+
+export function libelleCategorie(categorie: string) {
+  return CATEGORIE_LABELS[categorie] ?? categorie;
+}
