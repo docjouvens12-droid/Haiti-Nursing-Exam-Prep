2c388728a067510a0cf88e536f89537884edd06b
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-06 03:50:26 -0600

    Remove obsolete teacher assessment observer

--- app/portail-scolaire/layout.tsx
+++ app/portail-scolaire/layout.tsx
@@ -6,7 +6,6 @@ import StudentAccessPanel from './StudentAccessPanel'
 import TeacherAccessPanel from './TeacherAccessPanel'
 import StudentTrimesterSummary from './StudentTrimesterSummary'
 import StudentWeightedBulletin from './StudentWeightedBulletin'
-import TeacherAssessmentSelector from './TeacherAssessmentSelector'
 import DirectionPublishSelector from './DirectionPublishSelector'
 import SessionJwtRecovery from './SessionJwtRecovery'
 import RecordsPanel from './RecordsPanel'
@@ -127,7 +126,6 @@ export default function PortailScolaireLayout({ children }: { children: ReactNod
       <StudentWeightedBulletin />
       <RecordsPanel />
       <AcademicYearEditor />
-      <TeacherAssessmentSelector />
       <DirectionPublishSelector />
       <SchoolIdentitySettings />
       <AllSectionsSelectorFix />
