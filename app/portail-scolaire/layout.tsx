3f2b4b58e89d441d508aca2868509b0fb6959dfa
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-05 19:37:00 -0600

    Add secretary language sync and academic controls

--- app/portail-scolaire/layout.tsx
+++ app/portail-scolaire/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 import SecretaryPanel from './SecretaryPanel'
 import LoginRoleLabelFix from './LoginRoleLabelFix'
+import SecretaryEnhancements from './SecretaryEnhancements'
 
 export default function PortailScolaireLayout({ children }: { children: ReactNode }) {
   return (
@@ -32,6 +33,7 @@ export default function PortailScolaireLayout({ children }: { children: ReactNod
       `}</style>
       {children}
       <SecretaryPanel />
+      <SecretaryEnhancements />
       <LoginRoleLabelFix />
     </>
   )
