b701152304ececa6c4a8820161df4123e87ee5b4
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-07 16:55:01 -0600

    Use simplified Taxi login title

--- taxi-app/app/layout.tsx
+++ taxi-app/app/layout.tsx
@@ -17,7 +17,7 @@ import PassengerHomePolish from '../components/PassengerHomePolish'
 import PassengerMenuPolish from '../components/PassengerMenuPolish'
 import AuthRoleRedirector from '../components/AuthRoleRedirector'
 import PasswordVisibilityToggle from '../components/PasswordVisibilityToggle'
-import LoginCopyNeutralizer from '../components/LoginCopyNeutralizer'
+import LoginTitlePolish from '../components/LoginTitlePolish'
 
 export const metadata: Metadata = {
   title: 'Taxi Platform Haiti',
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="fr">
-      <body><PasswordVisibilityToggle /><LoginCopyNeutralizer /><AuthRoleRedirector /><HaitiTestGeolocation /><DriverCtaBridge /><GlobalSpacesButton /><GlobalLogoutButton /><PassengerRideCompletion /><PassengerActiveDriver /><PassengerPendingRideCancel /><RideRealtimeNotifications /><DriverDashboardTitleCleanup /><DriverMenuPersonalInfoCleanup /><PassengerHomePolish /><PassengerMenuPolish />{children}</body>
+      <body><PasswordVisibilityToggle /><LoginTitlePolish /><AuthRoleRedirector /><HaitiTestGeolocation /><DriverCtaBridge /><GlobalSpacesButton /><GlobalLogoutButton /><PassengerRideCompletion /><PassengerActiveDriver /><PassengerPendingRideCancel /><RideRealtimeNotifications /><DriverDashboardTitleCleanup /><DriverMenuPersonalInfoCleanup /><PassengerHomePolish /><PassengerMenuPolish />{children}</body>
     </html>
   )
 }
