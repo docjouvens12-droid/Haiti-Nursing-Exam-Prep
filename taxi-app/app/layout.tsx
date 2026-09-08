23fba447a0556b824414eda1a5d6963d4a534f17
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-07 23:20:03 -0600

    Load passenger trip experience polish

--- taxi-app/app/layout.tsx
+++ taxi-app/app/layout.tsx
@@ -25,6 +25,7 @@ import PassengerAvatarUploadPolish from '../components/PassengerAvatarUploadPoli
 import PassengerMenuFinalConsistency from '../components/PassengerMenuFinalConsistency'
 import PassengerDashboardDriverStyle from '../components/PassengerDashboardDriverStyle'
 import PassengerDashboardQuickInfo from '../components/PassengerDashboardQuickInfo'
+import PassengerTripExperiencePolish from '../components/PassengerTripExperiencePolish'
 import DriverMenuAccordionPolish from '../components/DriverMenuAccordionPolish'
 import DriverAvatarUploadPolish from '../components/DriverAvatarUploadPolish'
 import DriverVehicleMenuPolish from '../components/DriverVehicleMenuPolish'
@@ -55,7 +56,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="fr">
-      <body><PasswordVisibilityToggle /><LoginTitlePolish /><AuthRoleRedirector /><HaitiTestGeolocation /><DriverCtaBridge /><GlobalSpacesButton /><GlobalLogoutButton /><PassengerRideCompletion /><PassengerActiveDriver /><PassengerPendingRideCancel /><RideRealtimeNotifications /><PassengerHomePolish /><PassengerBookingFlowPolish /><PassengerMobilePaymentPolish /><PassengerMenuPolish /><PassengerMenuCompactPolish /><PassengerLanguageLogoutPolish /><PassengerLanguageStayPut /><PassengerRideHistoryMenuPolish /><PassengerHelpDirectNavigation /><PassengerNativeHelpPanelPolish /><PassengerAvatarUploadPolish /><PassengerMenuFinalConsistency /><PassengerDashboardDriverStyle /><PassengerDashboardQuickInfo /><DriverMenuAccordionPolish /><DriverAvatarUploadPolish /><DriverVehicleMenuPolish /><DriverLanguageSwitchPolish /><DriverDashboardTitleHide /><DriverOnlineSwitchPolish /><DriverBrandTaxiPolish /><DriverCompactBrandPolish /><DriverEarningsMenuPolish /><DriverNetEarningsPolish /><DriverRideHistoryMenuPolish /><DriverMenuVisualConsistency /><DriverLogoutPolish /><DriverAutoRequestSearch /><DriverRideExperiencePolish /><DriverCompletedRideSummary /><DriverPaymentMethodPolish /><DriverPaymentMenuPolish />{children}</body>
+      <body><PasswordVisibilityToggle /><LoginTitlePolish /><AuthRoleRedirector /><HaitiTestGeolocation /><DriverCtaBridge /><GlobalSpacesButton /><GlobalLogoutButton /><PassengerRideCompletion /><PassengerActiveDriver /><PassengerPendingRideCancel /><RideRealtimeNotifications /><PassengerHomePolish /><PassengerBookingFlowPolish /><PassengerMobilePaymentPolish /><PassengerMenuPolish /><PassengerMenuCompactPolish /><PassengerLanguageLogoutPolish /><PassengerLanguageStayPut /><PassengerRideHistoryMenuPolish /><PassengerHelpDirectNavigation /><PassengerNativeHelpPanelPolish /><PassengerAvatarUploadPolish /><PassengerMenuFinalConsistency /><PassengerDashboardDriverStyle /><PassengerDashboardQuickInfo /><PassengerTripExperiencePolish /><DriverMenuAccordionPolish /><DriverAvatarUploadPolish /><DriverVehicleMenuPolish /><DriverLanguageSwitchPolish /><DriverDashboardTitleHide /><DriverOnlineSwitchPolish /><DriverBrandTaxiPolish /><DriverCompactBrandPolish /><DriverEarningsMenuPolish /><DriverNetEarningsPolish /><DriverRideHistoryMenuPolish /><DriverMenuVisualConsistency /><DriverLogoutPolish /><DriverAutoRequestSearch /><DriverRideExperiencePolish /><DriverCompletedRideSummary /><DriverPaymentMethodPolish /><DriverPaymentMenuPolish />{children}</body>
     </html>
   )
 }
