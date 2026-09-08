1032e94e147c757ea47b2e69aae701225cdc9f30
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-08 15:26:20 -0600

    Remove duplicate driver ride offers panel

--- taxi-app/app/driver/dashboard/layout.tsx
+++ taxi-app/app/driver/dashboard/layout.tsx
@@ -1,11 +1,5 @@
 import type { ReactNode } from 'react'
-import DriverOpenRideOffers from '../../../components/DriverOpenRideOffers'
 
 export default function DriverDashboardLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <DriverOpenRideOffers />
-      {children}
-    </>
-  )
+  return children
 }
