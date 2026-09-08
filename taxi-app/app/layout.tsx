ec7c574b32ae7de6e02218f9d4952b65d6a2e524
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-07 22:14:35 -0600

    Load live passenger AI driver info helper

--- taxi-app/app/layout.tsx
+++ taxi-app/app/layout.tsx
@@ -21,6 +21,7 @@ import PassengerRideHistoryMenuPolish from '../components/PassengerRideHistoryMe
 import PassengerHelpMenuPolish from '../components/PassengerHelpMenuPolish'
 import PassengerSupportRequestPolish from '../components/PassengerSupportRequestPolish'
 import PassengerHelpAdvancedPolish from '../components/PassengerHelpAdvancedPolish'
+import PassengerAiDriverInfoPolish from '../components/PassengerAiDriverInfoPolish'
 import PassengerAvatarUploadPolish from '../components/PassengerAvatarUploadPolish'
 import DriverMenuAccordionPolish from '../components/DriverMenuAccordionPolish'
 import DriverAvatarUploadPolish from '../components/DriverAvatarUploadPolish'
@@ -52,7 +53,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="fr">
-      <body><PasswordVisibilityToggle /><LoginTitlePolish /><AuthRoleRedirector /><HaitiTestGeolocation /><DriverCtaBridge /><GlobalSpacesButton /><GlobalLogoutButton /><PassengerRideCompletion /><PassengerActiveDriver /><PassengerPendingRideCancel /><RideRealtimeNotifications /><PassengerHomePolish /><PassengerBookingFlowPolish /><PassengerMobilePaymentPolish /><PassengerMenuPolish /><PassengerMenuCompactPolish /><PassengerLanguageLogoutPolish /><PassengerRideHistoryMenuPolish /><PassengerHelpMenuPolish /><PassengerSupportRequestPolish /><PassengerHelpAdvancedPolish /><PassengerAvatarUploadPolish /><DriverMenuAccordionPolish /><DriverAvatarUploadPolish /><DriverVehicleMenuPolish /><DriverLanguageSwitchPolish /><DriverDashboardTitleHide /><DriverOnlineSwitchPolish /><DriverBrandTaxiPolish /><DriverCompactBrandPolish /><DriverEarningsMenuPolish /><DriverNetEarningsPolish /><DriverRideHistoryMenuPolish /><DriverMenuVisualConsistency /><DriverLogoutPolish /><DriverAutoRequestSearch /><DriverRideExperiencePolish /><DriverCompletedRideSummary /><DriverPaymentMethodPolish /><DriverPaymentMenuPolish />{children}</body>
+      <body><PasswordVisibilityToggle /><LoginTitlePolish /><AuthRoleRedirector /><HaitiTestGeolocation /><DriverCtaBridge /><GlobalSpacesButton /><GlobalLogoutButton /><PassengerRideCompletion /><PassengerActiveDriver /><PassengerPendingRideCancel /><RideRealtimeNotifications /><PassengerHomePolish /><PassengerBookingFlowPolish /><PassengerMobilePaymentPolish /><PassengerMenuPolish /><PassengerMenuCompactPolish /><PassengerLanguageLogoutPolish /><PassengerRideHistoryMenuPolish /><PassengerHelpMenuPolish /><PassengerSupportRequestPolish /><PassengerHelpAdvancedPolish /><PassengerAiDriverInfoPolish /><PassengerAvatarUploadPolish /><DriverMenuAccordionPolish /><DriverAvatarUploadPolish /><DriverVehicleMenuPolish /><DriverLanguageSwitchPolish /><DriverDashboardTitleHide /><DriverOnlineSwitchPolish /><DriverBrandTaxiPolish /><DriverCompactBrandPolish /><DriverEarningsMenuPolish /><DriverNetEarningsPolish /><DriverRideHistoryMenuPolish /><DriverMenuVisualConsistency /><DriverLogoutPolish /><DriverAutoRequestSearch /><DriverRideExperiencePolish /><DriverCompletedRideSummary /><DriverPaymentMethodPolish /><DriverPaymentMenuPolish />{children}</body>
     </html>
   )
 }
