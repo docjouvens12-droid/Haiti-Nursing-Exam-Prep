08fb0dedb427122bc94cbadeed38df0b6a76e514
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-08 19:44:19 -0600

    Load passenger avatar menu enhancements

--- taxi-app/app/layout.tsx
+++ taxi-app/app/layout.tsx
@@ -5,9 +5,11 @@ import './completion.css'
 import './passenger-secondary.css'
 import './passenger-dashboard-mobile.css'
 import './passenger-menu-final.css'
+import './passenger-avatar.css'
 import PassengerActiveDriver from '../components/PassengerActiveDriver'
 import PassengerPendingRideCancel from '../components/PassengerPendingRideCancel'
 import PassengerRideCompletion from '../components/PassengerRideCompletion'
+import PassengerAvatarEnhancer from '../components/PassengerAvatarEnhancer'
 
 export const metadata: Metadata = {
   title: 'Taxi Platform Haiti',
@@ -21,6 +23,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <PassengerActiveDriver />
         <PassengerPendingRideCancel />
         <PassengerRideCompletion />
+        <PassengerAvatarEnhancer />
         {children}
       </body>
     </html>
