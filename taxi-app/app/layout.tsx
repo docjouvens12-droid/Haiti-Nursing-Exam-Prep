768a8d3110221d1a603b7162ec335af8c2d70451
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-08 22:21:16 -0600

    Mount same-page passenger login fix

--- taxi-app/app/layout.tsx
+++ taxi-app/app/layout.tsx
@@ -24,6 +24,7 @@ import PassengerDashboardDriverStyle from '../components/PassengerDashboardDrive
 import PassengerMainPaymentPolish from '../components/PassengerMainPaymentPolish'
 import PassengerDashboardQuickInfo from '../components/PassengerDashboardQuickInfo'
 import PassengerRestoredVersionPolish from '../components/PassengerRestoredVersionPolish'
+import PassengerSamePageLoginFix from '../components/PassengerSamePageLoginFix'
 import DriverMenuAccordionPolish from '../components/DriverMenuAccordionPolish'
 import DriverAvatarUploadPolish from '../components/DriverAvatarUploadPolish'
 import DriverVehicleMenuPolish from '../components/DriverVehicleMenuPolish'
@@ -66,6 +67,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <PassengerMainPaymentPolish />
         <PassengerDashboardQuickInfo />
         <PassengerRestoredVersionPolish />
+        <PassengerSamePageLoginFix />
         <DriverMenuAccordionPolish />
         <DriverAvatarUploadPolish />
         <DriverVehicleMenuPolish />
