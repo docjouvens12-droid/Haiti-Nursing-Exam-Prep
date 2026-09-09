14e69348ffb22db0b5179cc31658bd676da6e50c
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-09 00:42:50 -0600

    Mount driver hamburger tap recovery

--- taxi-app/app/layout.tsx
+++ taxi-app/app/layout.tsx
@@ -38,6 +38,7 @@ import DriverApplicationProfileAddon from '../components/DriverApplicationProfil
 import DriverProfileRequiredFields from '../components/DriverProfileRequiredFields'
 import DriverVehicleRequiredFieldsGuard from '../components/DriverVehicleRequiredFieldsGuard'
 import DriverHelpTopicsAddon from '../components/DriverHelpTopicsAddon'
+import DriverMenuTapRecovery from '../components/DriverMenuTapRecovery'
 import AdminDriverApplicationProfileSnapshot from '../components/AdminDriverApplicationProfileSnapshot'
 import AdminDriverVehicleSnapshot from '../components/AdminDriverVehicleSnapshot'
 
@@ -78,6 +79,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <DriverProfileRequiredFields />
         <DriverVehicleRequiredFieldsGuard />
         <DriverHelpTopicsAddon />
+        <DriverMenuTapRecovery />
         <AdminDriverApplicationProfileSnapshot />
         <AdminDriverVehicleSnapshot />
         {children}
