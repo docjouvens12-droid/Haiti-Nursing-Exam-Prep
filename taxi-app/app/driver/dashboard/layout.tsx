a4c22e076802644857610ca934defcfbb42650bf
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-11 12:58:05 -0600

    Remove conflicting driver dashboard hydrator

--- taxi-app/app/driver/dashboard/layout.tsx
+++ taxi-app/app/driver/dashboard/layout.tsx
@@ -1,10 +1,6 @@
 import type { ReactNode } from 'react'
 import './driver-dashboard.css'
-import DriverDashboardHydrator from '../../../components/DriverDashboardHydrator'
 
 export default function DriverDashboardLayout({ children }: { children: ReactNode }) {
-  return <>
-    {children}
-    <DriverDashboardHydrator />
-  </>
+  return <>{children}</>
 }
