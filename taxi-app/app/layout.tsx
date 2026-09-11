2d8f3191869b999e232f4486afc766c13db4e3bb
docjouvens12-droid <docjouvens12@gmail.com>
2026-09-11 02:33:18 -0600

    Separate passenger driver and admin UI by route

--- taxi-app/app/layout.tsx
+++ taxi-app/app/layout.tsx
@@ -22,57 +22,7 @@ import AdminPermissionGuard from '../components/AdminPermissionGuard'
 import PasswordVisibilityToggle from '../components/PasswordVisibilityToggle'
 import PwaRegister from '../components/PwaRegister'
 import HaitiTestGeolocation from '../components/HaitiTestGeolocation'
-import PassengerActiveDriver from '../components/PassengerActiveDriver'
-import PassengerPendingRideCancel from '../components/PassengerPendingRideCancel'
-import PassengerRideCompletion from '../components/PassengerRideCompletion'
-import PassengerStaleReceiptGuard from '../components/PassengerStaleReceiptGuard'
-import PassengerRidePhasePolish from '../components/PassengerRidePhasePolish'
-import PassengerCompletionPolish from '../components/PassengerCompletionPolish'
-import PassengerRideLifecycleGuard from '../components/PassengerRideLifecycleGuard'
-import PassengerAvatarEnhancer from '../components/PassengerAvatarEnhancer'
-import PassengerProfileDetails from '../components/PassengerProfileDetails'
-import PassengerDriverApplicationMenuItem from '../components/PassengerDriverApplicationMenuItem'
-import PassengerTripsEnhancer from '../components/PassengerTripsEnhancer'
-import PassengerPaymentEnhancer from '../components/PassengerPaymentEnhancer'
-import PassengerLanguageSwitchEnhancer from '../components/PassengerLanguageSwitchEnhancer'
-import PassengerHelpDirectNavigation from '../components/PassengerHelpDirectNavigation'
-import PassengerLogoutPolish from '../components/PassengerLogoutPolish'
-import PassengerRidePaymentSync from '../components/PassengerRidePaymentSync'
-import PassengerDashboardDriverStyle from '../components/PassengerDashboardDriverStyle'
-import PassengerMainPaymentPolish from '../components/PassengerMainPaymentPolish'
-import PassengerDashboardQuickInfo from '../components/PassengerDashboardQuickInfo'
-import PassengerRestoredVersionPolish from '../components/PassengerRestoredVersionPolish'
-import PassengerHomeDashboardPolish from '../components/PassengerHomeDashboardPolish'
-import PassengerDashboardV2Polish from '../components/PassengerDashboardV2Polish'
-import PassengerDashboardHardV2 from '../components/PassengerDashboardHardV2'
-import PassengerDashboardFinalReview from '../components/PassengerDashboardFinalReview'
-import PassengerPlatformRedesignV3 from '../components/PassengerPlatformRedesignV3'
-import PassengerUnifiedFinalPolish from '../components/PassengerUnifiedFinalPolish'
-import MoviPassengerDesign from '../components/MoviPassengerDesign'
-import DriverAvatarUploadPolish from '../components/DriverAvatarUploadPolish'
-import DriverDashboardTitleHide from '../components/DriverDashboardTitleHide'
-import DriverOnlineSwitchPolish from '../components/DriverOnlineSwitchPolish'
-import DriverBrandTaxiPolish from '../components/DriverBrandTaxiPolish'
-import DriverCompactBrandPolish from '../components/DriverCompactBrandPolish'
-import DriverLogoutPolish from '../components/DriverLogoutPolish'
-import DriverAutoRequestSearch from '../components/DriverAutoRequestSearch'
-import DriverRideExperiencePolish from '../components/DriverRideExperiencePolish'
-import DriverCompletedRideSummary from '../components/DriverCompletedRideSummary'
-import DriverHomeDashboardPolish from '../components/DriverHomeDashboardPolish'
-import DriverFinalMenuStable from '../components/DriverFinalMenuStable'
-import DriverApplicationSubmitButton from '../components/DriverApplicationSubmitButton'
-import DriverPayoutEditPolish from '../components/DriverPayoutEditPolish'
-import DriverFinalHelpTopics from '../components/DriverFinalHelpTopics'
-import DriverDrawerHeaderPolish from '../components/DriverDrawerHeaderPolish'
-import DriverProfileSectionPolish from '../components/DriverProfileSectionPolish'
-import DriverVehicleSectionPolish from '../components/DriverVehicleSectionPolish'
-import DriverPaymentsSectionPolish from '../components/DriverPaymentsSectionPolish'
-import DriverHistorySectionPolish from '../components/DriverHistorySectionPolish'
-import DriverEarningsSectionPolish from '../components/DriverEarningsSectionPolish'
-import DriverLanguageSectionPolish from '../components/DriverLanguageSectionPolish'
-import DriverAccessGate from '../components/DriverAccessGate'
-import AdminDriverApplicationProfileSnapshot from '../components/AdminDriverApplicationProfileSnapshot'
-import AdminDriverVehicleSnapshot from '../components/AdminDriverVehicleSnapshot'
+import RouteScopedEnhancers from '../components/RouteScopedEnhancers'
 
 export const metadata: Metadata = {
   title: 'MOVI',
@@ -111,57 +61,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <AuthRoleRedirector />
         <AdminPermissionGuard />
         <PasswordVisibilityToggle />
-        <PassengerRideLifecycleGuard />
-        <PassengerActiveDriver />
-        <PassengerPendingRideCancel />
-        <PassengerRideCompletion />
-        <PassengerStaleReceiptGuard />
-        <PassengerRidePhasePolish />
-        <PassengerCompletionPolish />
-        <PassengerAvatarEnhancer />
-        <PassengerProfileDetails />
-        <PassengerDriverApplicationMenuItem />
-        <PassengerTripsEnhancer />
-        <PassengerPaymentEnhancer />
-        <PassengerLanguageSwitchEnhancer />
-        <PassengerHelpDirectNavigation />
-        <PassengerLogoutPolish />
-        <PassengerRidePaymentSync />
-        <PassengerDashboardDriverStyle />
-        <PassengerMainPaymentPolish />
-        <PassengerDashboardQuickInfo />
-        <PassengerRestoredVersionPolish />
-        <PassengerHomeDashboardPolish />
-        <PassengerDashboardV2Polish />
-        <PassengerDashboardHardV2 />
-        <PassengerDashboardFinalReview />
-        <PassengerPlatformRedesignV3 />
-        <PassengerUnifiedFinalPolish />
-        <MoviPassengerDesign />
-        <DriverAccessGate />
-        <DriverAvatarUploadPolish />
-        <DriverDashboardTitleHide />
-        <DriverOnlineSwitchPolish />
-        <DriverBrandTaxiPolish />
-        <DriverCompactBrandPolish />
-        <DriverLogoutPolish />
-        <DriverAutoRequestSearch />
-        <DriverRideExperiencePolish />
-        <DriverCompletedRideSummary />
-        <DriverHomeDashboardPolish />
-        <DriverFinalMenuStable />
-        <DriverApplicationSubmitButton />
-        <DriverPayoutEditPolish />
-        <DriverFinalHelpTopics />
-        <DriverDrawerHeaderPolish />
-        <DriverProfileSectionPolish />
-        <DriverVehicleSectionPolish />
-        <DriverPaymentsSectionPolish />
-        <DriverHistorySectionPolish />
-        <DriverEarningsSectionPolish />
-        <DriverLanguageSectionPolish />
-        <AdminDriverApplicationProfileSnapshot />
-        <AdminDriverVehicleSnapshot />
+        <RouteScopedEnhancers />
         {children}
       </body>
     </html>
